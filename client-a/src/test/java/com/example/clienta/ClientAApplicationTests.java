9d1bb8c67b7e84d0fc4c2870bbf8270424691cc5
Larry <tgfc045@tgfc.tw>
2019-11-27 17:43:09 +0800

    zzz

---
+++ client-a/src/test/java/com/example/clienta/ClientAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clienta;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
